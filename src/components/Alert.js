611aa448d7a51aee90eee819b00f09348b396326
Anshulsharma82 <anshulsharma4445@gmail.com>
2024-05-04 13:34:15 +0530

    minor changes

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,13 +6,14 @@ function capitalize(str) {
     return lowerCase[0].toUpperCase() + lowerCase.slice(1)
 }
 
-export default function Alert(props){
-    return(
-        props.alert && <>
-        <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{capitalize(props?.alert?.type)}</strong>: {props?.alert?.msg}
-            
+export default function Alert(props) {
+    return (
+
+        <div style={{height: '50px'}}>
+            {props.alert &&
+            <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capitalize(props?.alert?.type)}</strong>: {props?.alert?.msg}
+            </div>}
         </div>
-        </>
     )
 }
